8a83b68d8138a829dfa5ea9185750554b7a7d2fb
Hirerov <37737923+Hirerov@users.noreply.github.com>
2019-03-23 16:28:56 +0100

    Dodanie dodatkowego rozwiązania dla NEHa
    Dodanie dodatkowego rozwiązania dla NEHa i sprawdzenie statystyk

--- kod.py
+++ kod.py
@@ -44,10 +44,10 @@ if wybor == "1":
     print("Skonczylem losowac.\n")
 
 else:
-    print("Zrezygnowano z losowania. Wczytuje pliki z txt")
+    print("Zrezygnowano z losowania. Wczytuje dane z txt")
 
 
-start = time.time() #czas obliczania wykonywania sie algorytmu
+
 
 ilosc_zadan = int(tekst[0])
 ilosc_maszyn = int(tekst[1])
@@ -69,7 +69,6 @@ for x in range (ilosc_zadan):
 kolejka_zadan=[]
 for x in range(ilosc_zadan):
     kolejka_zadan.append(x)
-print(kolejka_zadan)
 
 ###################################
 ###################################
@@ -96,6 +95,9 @@ def calc_cmax(list_tmp):
     #print('taskarray',taskarray) #diagnostyka
 
     machine1, machine2, machine3, machine4, machine5 = 0, 0, 0, 0, 0
+    machine6,machine7,machine8,machine9,machine10 = 0, 0, 0, 0, 0
+    machine11, machine12, machine13, machine14, machine15 = 0, 0, 0, 0, 0
+    machine16, machine17, machine18, machine19, machine20 = 0, 0, 0, 0, 0
 
     for x in range(0, n):
         machine1 += taskarray[x][1]
@@ -104,6 +106,21 @@ def calc_cmax(list_tmp):
             machine3 = machine2 + taskarray[x][3]
             machine4 = machine3 + taskarray[x][4]
             machine5 = machine4 + taskarray[x][5]
+            machine6 = machine5 + taskarray[x][6]
+            machine7 = machine6 + taskarray[x][7]
+            machine8 = machine7 + taskarray[x][8]
+            machine9 = machine8 + taskarray[x][9]
+            machine10 = machine9 + taskarray[x][10]
+            machine11 = machine10 + taskarray[x][11]
+            machine12 = machine11 + taskarray[x][12]
+            machine13 = machine12 + taskarray[x][13]
+            machine14 = machine13 + taskarray[x][14]
+            machine15 = machine14 + taskarray[x][15]
+            machine16 = machine15 + taskarray[x][16]
+            machine17 = machine16 + taskarray[x][17]
+            machine18 = machine17 + taskarray[x][18]
+            machine19 = machine18 + taskarray[x][19]
+            machine20 = machine19 + taskarray[x][20]
         else:
             if machine1 <= machine2:
                 machine2 += taskarray[x][2]
@@ -122,8 +139,82 @@ def calc_cmax(list_tmp):
             else:
                 machine5 = machine4 + taskarray[x][5]
 
+            if machine5 <= machine6:
+                machine6 += taskarray[x][6]
+            else:
+                machine6 = machine5 + taskarray[x][6]
+
+            if machine6 <= machine7:
+                machine7 += taskarray[x][7]
+            else:
+                machine7 = machine6 + taskarray[x][7]
+
+            if machine7 <= machine8:
+                machine8 += taskarray[x][8]
+            else:
+                machine8 = machine7 + taskarray[x][8]
+
+            if machine8 <= machine9:
+                machine9 += taskarray[x][9]
+            else:
+                machine9 = machine8 + taskarray[x][9]
+
+            if machine9 <= machine10:
+                machine10 += taskarray[x][10]
+            else:
+                machine10 = machine9 + taskarray[x][10]
+
+            if machine10 <= machine11:
+                machine11 += taskarray[x][11]
+            else:
+                machine11 = machine10 + taskarray[x][11]
+
+            if machine11 <= machine12:
+                machine12 += taskarray[x][12]
+            else:
+                machine12 = machine11 + taskarray[x][12]
+
+            if machine12 <= machine13:
+                machine13 += taskarray[x][13]
+            else:
+                machine13 = machine12 + taskarray[x][13]
+
+            if machine13 <= machine14:
+                machine14 += taskarray[x][14]
+            else:
+                machine14 = machine13 + taskarray[x][14]
+
+            if machine14 <= machine15:
+                machine15 += taskarray[x][15]
+            else:
+                machine15 = machine14 + taskarray[x][15]
+
+            if machine15 <= machine16:
+                machine16 += taskarray[x][16]
+            else:
+                machine16 = machine15 + taskarray[x][16]
+
+            if machine16 <= machine17:
+                machine17 += taskarray[x][17]
+            else:
+                machine17 = machine16 + taskarray[x][17]
+
+            if machine17 <= machine18:
+                machine18 += taskarray[x][18]
+            else:
+                machine18 = machine17 + taskarray[x][18]
+
+            if machine18 <= machine19:
+                machine19 += taskarray[x][19]
+            else:
+                machine19 = machine18 + taskarray[x][19]
+
+            if machine19 <= machine20:
+                machine20 += taskarray[x][20]
+            else:
+                machine20 = machine19 + taskarray[x][20]
     #print("Czas wykonywania to:",machine5) #diagnostyka
-    return machine5 #narazie zawsze machine5, ale trzeba to zrobic uniwersalne
+    return machine20 #narazie zawsze machine5, ale trzeba to zrobic uniwersalne
 
 
 def find_max(list_neh,all_czas_zad):
@@ -158,6 +249,8 @@ def find_max(list_neh,all_czas_zad):
     return max_time
 
 def nr_zadania(list_neh,znaleziony_maks,all_czas_zad):
+    indeks_zad = 0 #deklaracja zm w funkcji
+
     if all_czas_zad.count(znaleziony_maks) > 1: # jesli dany czas wystepuje wiecej niz 1. raz na liscie sumy czasu zadan
         lista_tmp1 = copy.deepcopy(all_czas_zad)
         lista_tmp2=[]
@@ -170,15 +263,15 @@ def nr_zadania(list_neh,znaleziony_maks,all_czas_zad):
             if list_neh.count(lista_tmp2[x]) == 0:
                 indeks_zad = lista_tmp2[x]
                 break
-    else:
+    else: #jesli dany czas wystepuje jeden raz na liscie
         indeks_zad=all_czas_zad.index(znaleziony_maks)
     return indeks_zad
-
 
 def alg_neh(ilosc_zad):
     list_tmp=[]
     list_neh=[]
     all_czas_zad = []
+    wykonanie=0
 
     #zsumuj wszystkie czasy na maszynach dla poszczegolnych zadani i uwtorz liste
     for x in range(ilosc_zadan):
@@ -215,22 +308,142 @@ def alg_neh(ilosc_zad):
             list_tmp.remove(nr_akt_zad)       #skasuj nr akt. zadania z kolejki tmp, aby w wyniku wykonywania
             # petliu for wstawic go w inne miejsce (list_tmp.insert)
 
-        print('[END] LIST NEH', list_tmp)
+        #print('[END] LIST NEH i czas dla all',optymalne_czas, list_tmp)
         #print('[END] TIME AND LIST NEH', time1,list_tmp)
 
         list_neh.insert(time1.index(optymalne_czas), optymalne_zad) #umiesc na liscie NEH najlepsze przeanalizowane
                                                                     #zadanie wraz z jego najlepszym najlepszym
                                                                     #ustawieniem,aby uzyskac optymalny czas
-        # a=input() #diagnostyka i "zatrzymywanie" petli
+        wykonanie += 1
+        #print(wykonanie,'[END] KONIEC NORM ALG', calc_cmax(list_neh), list_neh)
+        #a=input() #diagnostyka i "zatrzymywanie" petli
 
         ###########w tym miejscu algorytm NEH juz sie wykonal ################
+
+    czas1= calc_cmax(list_neh)
     for x in range (len(list_neh)): #ostatecznie wypisz kolejke zadan z +1 (bo alg liczy od 0!)
         list_neh[x]+=1
+    print('\nCzas wykonywania zadan na wszystkich maszynach dla alg NEH bez modyfikacji to:',czas1)
     print('Lista kolejki zadan otrzyma wg algorytmu NEH:\n',list_neh)
+    return czas1
+
+def alg_neh_dod_rozw(ilosc_zad):
+    list_tmp=[]
+    list_neh=[]
+    all_czas_zad = []
+
+    wykonanie=0 #zm. pomocnicza do obliczenia ile razy wykonal sie kod
+
+    #zsumuj wszystkie czasy na maszynach dla poszczegolnych zadan i uwtorz liste
+    for x in range(ilosc_zadan):
+        czas = 0
+        for y in range(ilosc_maszyn):
+            czas += maszyny[x][y]
+        all_czas_zad.append(czas)
+
+    while len(list_tmp) != int(ilosc_zad-1): #dopóki elementow na liscie nie bedzie tyle, co realnych zadan
+                                             #dopóty algorytm szuka rozwiazan
+
+        list_tmp=copy.deepcopy(list_neh)     #deepcopy uzyta, aby kopiowac, a nie ODWOLYWAC się do list/macierzy
+                                             #W tym miejscu przypisujemy na poczatku zapisana juz liste zadan wg NEH
+                                             #i do tej listy probnie bedziemy chcieli umieszczac kolejne zadania
+                                             #a nastepnie patrzec na rezultat
+
+        optymalne_zad = ilosc_zadan + 1      #do porwnania
+        optymalne_czas = 10000000            #mozna zastapic wyszukiwaniem maksa
+        time1=[] #do zliczania i porywnywania danych czasow w zaleznosci od tego, gdzie wstawimy aktualnie analizowane
+                 #zadanie
+
+        #print('BEGIN LIST TMP',list_tmp)    #diagnostyka
+
+        for x in range (len(list_neh)+1):
+            znaleziony_maks=find_max(list_neh,all_czas_zad) #znajdz i zapisz maksymalny czas danego zadania
+            nr_akt_zad=nr_zadania(list_neh,znaleziony_maks,all_czas_zad) # znajdz nr zadania na podstawie jego czasu
+            list_tmp.insert(x, nr_akt_zad) #umiesc analizowane zadania w dowolonym dostepnym miejscu w liscie tmp
+            #w celu pozniejszego sprawdzenia, ktory czas wykonywania bedzie najszybszy
+
+            time1.append(calc_cmax(list_tmp)) #oblicz i zapisz czas wykonywanie zadan dla aktualnej kolejki zadan
+            if optymalne_czas>time1[x]:       #jesli analizowany czas jest lepszy od innych do go zapamietaj
+                optymalne_zad=list_tmp[x]
+                optymalne_czas=time1[x]
+            list_tmp.remove(nr_akt_zad)       #skasuj nr akt. zadania z kolejki tmp, aby w wyniku wykonywania
+            # petliu for wstawic go w inne miejsce (list_tmp.insert)
 
 
-alg_neh(len(kolejka_zadan)) #wykonaj alg_neh tyle razy, co jest zadan
+        nasz_index=time1.index(optymalne_czas)
+        list_neh.insert(nasz_index, optymalne_zad) #umiesc na liscie NEH najlepsze przeanalizowane
+        wykonanie+=1 #licz ilosc krokow
+        #print(wykonanie,'[END] KONIEC NORM ALG', optymalne_czas, list_neh)
 
+
+        #############################################
+        #############################################
+        #############################################
+        #dodanie rozwiazania nr 4, czyli wybor i analiza
+        #zadania, którego usunięcie spowoduję najwieksze zmniejszenie wartości Cmax.
+
+        #zmienne do zapamietania najmniejszego cmaxa, gdy wyrzuci sie dane zadanie (rozw_dod_nr_zad o
+        #indeksie równym rozw_dod_zapisz_index)
+        best_time_normal, best_nr_zad, best_zapisz_index = 10000000000, 0, 0
+
+
+        for a in range(len(list_neh)): #petla wykonuje sie dla zadan w aktualanym NEHu procz:
+            if a != nasz_index:        #zadania, ktore zostalo ostatnio dodane (w wyoniku dzialania normalnego NEHa)
+                zapisz_nr_zad=list_neh.pop(a) #sciagnij indeks a z tablicy NEHa, a nastepnie zapisz nr zadania
+                best_time_act=calc_cmax(list_neh) #oblicz aktualny cmax
+                #print('czas akt',best_time_act,'dla braku zad',zapisz_nr_zad) #diagnostyka
+
+                if best_time_act < best_time_normal: #jesli cmax mniejszy niz aktualnie zapisany to zapamietaj dane
+                    best_time_normal=best_time_act
+                    best_nr_zad = zapisz_nr_zad
+                    best_zapisz_index=a
+                list_neh.insert(a,zapisz_nr_zad) #umiesc sciagniete wczesniej zadania z powrotem na liscie i analizuj
+
+        #print('czas akt', best_time_normal, 'dla braku zad', best_nr_zad)  # diagnostyka
+
+        if len(list_neh) != 1: #jesli lista NEH ma więcej w sobie zadan niz 1 to
+            time1 = []         #wyzruj tablice do zapisywania czasow
+            nr_zad=list_neh.pop(best_zapisz_index) #sciagnij z listy zadanie o wczesniej zapisanym indeksie i zapisz
+                                                   # nr zadania
+            optymalne_czas=1000000000
+            for b in range(len(list_neh) + 1):
+                list_neh.insert(b, nr_zad)
+                time1.append(calc_cmax(list_neh))
+                #print(list_neh,'dal ustawienia, czas taki:',calc_cmax(list_neh)) #diagnostyka
+                if optymalne_czas > time1[b]:
+                    best_nr_zad = list_neh[b]
+                    optymalne_czas = time1[b]
+                list_neh.remove(nr_zad)
+
+            list_neh.insert(time1.index(optymalne_czas),best_nr_zad)  # umiesc na liscie NEH najlepszy przypadek
+            #print('   [END] CZAS I LISTA PO', calc_cmax(list_neh), list_neh,'\n')
+        #sa=input() #diagnostyka
+
+        ###########w tym miejscu algorytm NEH juz sie wykonal ################
+
+    czas2= calc_cmax(list_neh)
+    for x in range (len(list_neh)): #ostatecznie wypisz kolejke zadan z +1 (bo alg liczy od 0!)
+        list_neh[x]+=1
+    print('',list_neh)
+    print('Lista kolejki zadan otrzyma wg algorytmu NEH z rozw. dod. widoczna u gory')
+    print('Czas wykonywania zadan na wszystkich maszynach dla NEH z rozw. dod. to:',czas2)
+    return czas2
+
+czas1,czas2=0,0
+start = time.time() #czas obliczania wykonywania sie algorytmu
+czas1=alg_neh(len(kolejka_zadan)) #wykonaj alg_neh tyle razy, co jest zadan
 end = time.time()
-print("Czas dzialania dla algorytmu NEH to: ", end-start,"s\n")
-print("Ilosc zadan to",ilosc_zadan,"a ilosc maszyn to",ilosc_maszyn)
+
+norm_neh=end-start
+
+start = time.time() #czas obliczania wykonywania sie algorytmu
+czas2=alg_neh_dod_rozw(len(kolejka_zadan)) #wykonaj alg_neh tyle razy, co jest zadan
+end = time.time()
+
+neh_dod_rozw=end-start
+
+print("\nIlosc zadan to",ilosc_zadan,"a ilosc maszyn to",ilosc_maszyn)
+print("Czas dzialania dla algorytmu NEH to:", norm_neh,"s")
+print("Czas dzialania dla algorytmu NEH z dod. rozw. to:", neh_dod_rozw,"s")
+print('Roznica dla czasow dzialania alg to:',neh_dod_rozw-norm_neh,'s')
+print('Roznica dla czasu wykonywania zadan na maszynach w zaleznosci od alg. to:',czas1-czas2)
