8789f4fe06d2d2727dab47aa993dce45ab9d7aa4
Hirerov <37737923+Hirerov@users.noreply.github.com>
2019-04-08 22:05:42 +0200

    Uporządkowanie komentarzy i kodu

--- zadanie 3/sa.py
+++ zadanie 3/sa.py
@@ -273,7 +273,7 @@ def calc_cmax(list_tmp):
 
 
 #Symulowane wyzarzanie
-def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
+def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor): #dl_kolejki,wybor temp,schladzanie,wybor insert(1),swap(2)
     print('Algorytm zaczyna prace!\n')
     print('Cmax best   Cmax aktualny')
 
@@ -281,51 +281,37 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
 
     #zmienne pomocnicze
     list_zad_los = []
-    #list_zad_los_tmp = []
-    old_best_kolejka=[]
+    list_zad_los_tmp = []
     bez_rezultatu=0
     ilosc_interacji=0 #poczatkowa ilosc iteracji
     ilosc_powtorzen=130000 # maks. ilosc iteracji
     wyb=2
-    # wybor=1 #wybor dzialania alg: 1 - insert, 2 - swap
-
-    #wybor temp wplywa jasno na ilosc ogolnej iteracji
-    # temperatura = 3e+400
-    # temperatura = 3e+300
-    # temperatura = 3e+1
-    # temperatura = 3e-100
-
-    # wspol_schladzania = 0.99 #wplywa na ilosc iteracji, czym mniejszy tym wiecej iteracji
-    # wspol_schladzania = 0.95
-    # wspol_schladzania = 0.9
-    # wspol_schladzania = 0.8
-    # wspol_schladzania = 0.4
-    # wspol_schladzania = 0.01
-    # wspol_schladzania = 0.01
 
     #etap 1 Inicjalizacja
-    for x in range (ilosc_zadan): #tworzenie listy zadan od 0 do tyle ile zadan
-        list_zad_los.append(x)
+    for x in range (ilosc_zadan): #tworzenie listy zadan od 0 do tyle ile
+        list_zad_los.append(x)    #!!!zadan po kolei!!!
 
     losuj.shuffle(list_zad_los) #losowe rozstawienie zadan
+
+    # lista z NEHa bez modyfikacji, ta_098, cmax - 10 829
+    # list_zad_los=[150, 42, 104, 18, 153, 167, 162, 179, 100, 178, 1, 155, 169, 130, 64, 46, 196, 183, 188, 65, 7, 82, 69, 59, 136, 117, 61, 15, 190, 25, 189, 91, 84, 86, 27, 128, 54, 23, 118, 115, 93, 101, 127, 140, 105, 44, 71, 28, 32, 72, 119, 6, 149, 20, 9, 50, 47, 30, 131, 194, 21, 133, 191, 8, 176, 158, 99, 121, 134, 94, 19, 79, 56, 109, 88, 125, 63, 80, 164, 70, 106, 22, 114, 159, 12, 53, 98, 39, 154, 185, 76, 36, 132, 186, 120, 116, 148, 129, 52, 192, 157, 58, 122, 137, 103, 49, 5, 14, 77, 163, 34, 171, 123, 112, 13, 40, 156, 31, 41, 85, 2, 38, 141, 139, 135, 161, 187, 173, 16, 126, 97, 68, 160, 35, 60, 145, 90, 78, 177, 146, 11, 174, 199, 26, 197, 4, 170, 172, 198, 144, 62, 138, 87, 193, 108, 51, 143, 43, 96, 55, 195, 29, 168, 45, 182, 75, 142, 92, 184, 102, 95, 83, 10, 147, 110, 124, 3, 111, 151, 73, 166, 175, 74, 180, 81, 67, 24, 165, 113, 48, 33, 89, 0, 17, 66, 37, 57, 152, 181, 107]
+
+    # lista z NEHa z modyfikacja, ta_098, cmax - 10 808
     # list_zad_los = [113, 57, 42, 153, 22, 37, 151, 18, 130, 180, 167, 111, 121, 123, 64, 117, 72, 74, 46, 150, 100, 65, 24, 81, 142, 169, 15, 1, 162, 84, 3, 196, 105, 124, 140, 34, 149, 136, 27, 49, 67, 163, 61, 69, 183, 91, 155, 190, 141, 86, 54, 9, 127, 44, 191, 110, 115, 187, 32, 186, 39, 48, 71, 14, 6, 128, 82, 77, 20, 28, 25, 119, 181, 7, 189, 188, 159, 17, 139, 118, 131, 176, 194, 51, 63, 50, 138, 164, 30, 116, 56, 12, 173, 87, 158, 26, 45, 23, 172, 112, 70, 157, 85, 83, 21, 76, 179, 106, 177, 132, 89, 120, 98, 92, 52, 137, 129, 198, 5, 88, 4, 103, 16, 175, 135, 161, 126, 185, 122, 41, 36, 94, 35, 160, 53, 31, 80, 60, 40, 19, 125, 2, 58, 96, 193, 114, 171, 146, 197, 109, 154, 68, 199, 38, 62, 192, 99, 145, 90, 55, 165, 13, 170, 144, 108, 174, 143, 79, 11, 8, 133, 97, 43, 78, 47, 195, 29, 168, 101, 148, 166, 93, 134, 178, 156, 102, 95, 10, 184, 59, 182, 75, 33, 66, 152, 73, 147, 104, 0, 107]
 
-    # list_zad_los=[150, 42, 104, 18, 153, 167, 162, 179, 100, 178, 1, 155, 169, 130, 64, 46, 196, 183, 188, 65, 7, 82, 69, 59, 136, 117, 61, 15, 190, 25, 189, 91, 84, 86, 27, 128, 54, 23, 118, 115, 93, 101, 127, 140, 105, 44, 71, 28, 32, 72, 119, 6, 149, 20, 9, 50, 47, 30, 131, 194, 21, 133, 191, 8, 176, 158, 99, 121, 134, 94, 19, 79, 56, 109, 88, 125, 63, 80, 164, 70, 106, 22, 114, 159, 12, 53, 98, 39, 154, 185, 76, 36, 132, 186, 120, 116, 148, 129, 52, 192, 157, 58, 122, 137, 103, 49, 5, 14, 77, 163, 34, 171, 123, 112, 13, 40, 156, 31, 41, 85, 2, 38, 141, 139, 135, 161, 187, 173, 16, 126, 97, 68, 160, 35, 60, 145, 90, 78, 177, 146, 11, 174, 199, 26, 197, 4, 170, 172, 198, 144, 62, 138, 87, 193, 108, 51, 143, 43, 96, 55, 195, 29, 168, 45, 182, 75, 142, 92, 184, 102, 95, 83, 10, 147, 110, 124, 3, 111, 151, 73, 166, 175, 74, 180, 81, 67, 24, 165, 113, 48, 33, 89, 0, 17, 66, 37, 57, 152, 181, 107]
     cmax_akt = calc_cmax(list_zad_los) #obliczenie cmaxa na poczatek
     cmax_best=cmax_akt #przypisanie cmaxa akt do best przy poczatku dzialania alg.
-    # print(cmax_akt,cmax_best)
 
     while ilosc_interacji != ilosc_powtorzen: #dopoki ilosc wykonania petli != ilosc_powtorzen, to dzialaj
         # etap2 generowanie ruchu
-        # insert
-        if wybor == 1:
+
+        if wybor == 1:   # insert
             list_zad_los_tmp=copy.deepcopy(list_zad_los) #kopiowanie (a nie wskaznik na liste!)
             zdejmij_zadanie = losuj.randint(0, ilosc_zadan - 1) #losowanie indeksu do zdjecia
             nr_zadania=list_zad_los_tmp.pop(zdejmij_zadanie) #zdjecie danego zadania z listy
             list_zad_los_tmp.insert(losuj.randint(0,ilosc_zadan-1),nr_zadania) #umiesc zdjete zadanie w losowym miejscu
 
-        # swap
-        elif wybor == 2:
+        elif wybor == 2: # swap
             list_zad_los_tmp = copy.deepcopy(list_zad_los)
             miejsce_1 = losuj.randint(0, ilosc_zadan - 1) #losowy wybor nr indeksow do swapa
             miejsce_2 = losuj.randint(0, ilosc_zadan - 1)
@@ -336,15 +322,16 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
             #operacja swap
             list_zad_los_tmp[miejsce_1],list_zad_los_tmp[miejsce_2]=list_zad_los_tmp[miejsce_2],list_zad_los_tmp[miejsce_1]
 
-        cmax_akt = calc_cmax(list_zad_los_tmp) #obliczenie cmaxa po operacji insert/swap
 
-        maks_ilosc_prob=20
+        # obliczenie cmaxa po operacji insert/swap
+        cmax_akt = calc_cmax(list_zad_los_tmp)
+
+        maks_ilosc_prob=30
         akt_ilosc_prob=0
 
         while (cmax_best == cmax_akt): #gdyby cmax po oepracji swpa/insert byl taki sam, to ponownie przeprowadz operacje
-                                     #az do skutku!
+                                       #insert/swap az do skutku!
             # print('camaxy takie same!')
-            # print('wysietl:', cmax_best, cmax_akt, akt_ilosc_prob)
             # insert
             if wybor == 1:
                 list_zad_los_tmp = copy.deepcopy(list_zad_los)
@@ -356,25 +343,18 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
                 list_zad_los_tmp = copy.deepcopy(list_zad_los)
                 miejsce_1 = losuj.randint(0, ilosc_zadan - 1)
                 miejsce_2 = losuj.randint(0, ilosc_zadan - 1)
-
                 while miejsce_1 == miejsce_2:  # gdyby oba zadania mialy taka sama wylosowana wartosc
                     miejsce_2 = losuj.randint(0, ilosc_zadan - 1)
-
                 list_zad_los_tmp[miejsce_1], list_zad_los_tmp[miejsce_2] = list_zad_los_tmp[miejsce_2], \
                                                                            list_zad_los_tmp[miejsce_1]
             cmax_akt = calc_cmax(list_zad_los_tmp) #na koniec petli policz nowy cmax_akt
-
-            # print('camaxy takie same!')
-            # print('wysietl:', cmax_best, cmax_akt, akt_ilosc_prob)
-
             akt_ilosc_prob += 1
-            if maks_ilosc_prob == akt_ilosc_prob:
-                cmax_akt+=1
-
+            if maks_ilosc_prob == akt_ilosc_prob: # jesli algorytm probuje ponad 30 razy bez skutku to:
+                cmax_akt+=1 #aby wyjsc z poszukiwania nowego cmaxa, przydaje sie przy skrajnie malej ilosci zadam/maszyn
 
         #etap 3 liczenie prawdopodobienstwa i decyzja o ruchu
-        # print(type(cmax_akt - cmax_best))
-        ################################### Boltzman ###################################
+
+        ################################### Dodatkowy algorytm I-MSAA (Boltzman) ###################################
         Boltzman =0
         war_do_prawd=0
         if wyb==2:
@@ -387,103 +367,85 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
                 if war_do_prawd > 709:
                     war_do_prawd=709 #limit liczenia e^(709)
 
-            # print(cmax_akt-cmax_best,temperatura,war_do_prawd)
-            # if war_do_prawd >= 0:
-            #     prawd=1 #bo, gdy war_do_prawd >=0 do exp(war_do_prawd) rowniez >=0
-            #     Boltzman=1
             if cmax_akt - cmax_best <= 0:
                 Boltzman = 1
             else:
                 Boltzman = 1/(1+2*math.exp(war_do_prawd))
-                #prawd = math.exp((cmax_akt - cmax_best) / temperatura)
 
-            if Boltzman > losuj.uniform(0,0.33):
+            if Boltzman > losuj.uniform(0,0.33): #warunek wykonania zamiany, jesli cmax_akt - cmax_best > 0
                     print(' ',cmax_best,'        ',cmax_akt,'  Iteracja nr {0}, wybor trybu {1}, wstawianie {2} '.format(ilosc_interacji,wyb,wybor))
                     cmax_best=cmax_akt
-                    old_best_kolejka=copy.deepcopy(list_zad_los)
                     list_zad_los=copy.deepcopy(list_zad_los_tmp)
-                    bez_rezultatu=1
+                    bez_rezultatu=1 #restart rezultatu
             else:
-                bez_rezultatu+=1
+                bez_rezultatu+=1 #jesli nie dokonano ifa (brak zmiany analizowanej kolejki), do rozpatrywanych dalej modyfikacji
 
 
-        # if bez_rezultatu == 30000:
-        #     print('zmiana tablic i bez_rezultatu=0')
-        #     list_zad_los = copy.deepcopy(old_best_kolejka)
-
-
-        # print((cmax_akt - cmax_best) / temperatura)
-        # print(math.exp((cmax_akt - cmax_best) / temperatura))
-
-        ################################### z intrukcji ###################################
-        if wyb ==1:
-            war_do_prawd=0
-            if temperatura < 9e-320: #break ze wzgledu na zbyt niska temp.
-                break
-            elif temperatura == 0: # gdyby temp. szybko przeskoczyla wartosc 9e-330 i stala sie 0
-                break
-            else:
-                war_do_prawd=(cmax_akt - cmax_best) / temperatura #do exp-onenty
-                if war_do_prawd > 709:
-                    war_do_prawd=709 #limit liczenia e^(709)
-
-            # print(cmax_akt-cmax_best,temperatura,war_do_prawd)
-            # if war_do_prawd >= 0:
-            #     prawd=1 #bo, gdy war_do_prawd >=0 do exp(war_do_prawd) rowniez >=0
-            #     Boltzman=1
-
-            if cmax_akt < cmax_best:
-                prawd=1
-            else:
-                prawd = math.exp(war_do_prawd)
-
-            if prawd >= losuj.uniform(0,1): #od pewnego momentu exp zawsze > 1 i dzieje sie bledne dzialnie
-                                            #co mozna zauwazyc podczas dzialania funkcji
-                print(' ',cmax_best,'        ',cmax_akt,'  Iteracja nr {0}, wybor trybu {1}, wstawianie {2} '.format(ilosc_interacji,wyb,wybor))
-                cmax_best=cmax_akt
-                old_best_kolejka=copy.deepcopy(list_zad_los)
-                list_zad_los=copy.deepcopy(list_zad_los_tmp)
-                bez_rezultatu=1
-            else:
-                bez_rezultatu+=1
-
-            # if math.fmod(bez_rezultatu,6500) == 0:
-            #     print('zmiana insert/swap','      Iteracja nr {0}'.format(ilosc_interacji))
-            #     if wybor == 1:
-            #         wybor=2
-            #     else:
-            #         wybor=1
-
-
-
-
-        # if bez_rezultatu == 40000:
-        #     print('zmiana tablic i bez_rezultatu=0')
-        #     list_zad_los = copy.deepcopy(old_best_kolejka)
-
-
-        # # etap 3 bazujacy na porownywaniu tylko cmaxow bez temp.
+        ################################### z intrukcji (PDF) ###################################
+        # if wyb ==1:
+        #     war_do_prawd=0
+        #     if temperatura < 9e-320: #break ze wzgledu na zbyt niska temp.
+        #         break
+        #     elif temperatura == 0: # gdyby temp. szybko przeskoczyla wartosc 9e-330 i stala sie 0
+        #         break
+        #     else:
+        #         war_do_prawd=(cmax_akt - cmax_best) / temperatura #do exp-onenty
+        #         if war_do_prawd > 709:
+        #             war_do_prawd=709 #limit liczenia e^(709)
+        #
+        #     if cmax_akt < cmax_best:
+        #         prawd=1
+        #     else:
+        #         prawd = math.exp(war_do_prawd)
+        #
+        #     if prawd >= losuj.uniform(0,1): #od pewnego momentu exp zawsze > 1 i dzieje sie bledne dzialnie
+        #                                     #co mozna zauwazyc podczas dzialania funkcji
+        #         print(' ',cmax_best,'        ',cmax_akt,'  Iteracja nr {0}, wybor trybu {1}, wstawianie {2} '.format(ilosc_interacji,wyb,wybor))
+        #         cmax_best=cmax_akt
+        #         list_zad_los=copy.deepcopy(list_zad_los_tmp)
+        #         bez_rezultatu=1
+        #     else:
+        #         bez_rezultatu+=1
+        ## etap 3 bazujacy na porownywaniu tylko cmaxow bez temp.
         # if cmax_akt < cmax_best:
         #     print(' ',cmax_best,'        ',cmax_akt)
         #     cmax_best=cmax_akt
         #     list_zad_los=copy.deepcopy(list_zad_los_tmp)
 
-        # if bez_rezultatu == 1:
-        #     # print('przelaczam na insert')
-        #     wybor = 1
-        # if bez_rezultatu == 20000 or (ilosc_interacji>70000 and bez_rezultatu > 5000):
-        #     wybor = 2
-        #     if bez_rezultatu == 20000:
-        #         print('przelaczam na swap')
+        #################
+        ################# etap 3.5 - wlasne modyfikacje (a raczej proby)
+        #################
+
+        ## proba modyfikacji nr 1
+        # if math.fmod(bez_rezultatu,6500) == 0:
+        #     print('zmiana insert/swap','      Iteracja nr {0}'.format(ilosc_interacji))
+        #     if wybor == 1:
+        #         wybor=2
+        #     else:
+        #         wybor=1
+
+        ## proba modyfikacji nr 2
+        # if bez_rezultatu == 40000:
+        #     print('zmiana tablic i bez_rezultatu=0')
+        #     list_zad_los = copy.deepcopy(old_best_kolejka) #trzeba zaimplementowac old_best_kolejka
+
+        ## proba modyfikacji nr 3 - DOSC UDANA
+        if bez_rezultatu == 1:
+            # print('przelaczam na insert')
+            wybor = 1
+        if bez_rezultatu == 20000 or (ilosc_interacji>70000 and bez_rezultatu > 5000):
+            wybor = 2
+            if bez_rezultatu == 20000:
+                print('przelaczam na swap')
+
+        ## proba modyfikacji nr 4 - DOSC UDANA
+        if bez_rezultatu > 40000 or ilosc_interacji>100000:
+            wybor = losuj.randint(1, 2)
+        if bez_rezultatu == 20000:
+            print('przelaczam na insert')
+            wybor = 1
         #
-        # # wybor=losuj.randint(1,2)
-        #
-        # if bez_rezultatu > 40000 or ilosc_interacji>100000:
-        #     wybor = losuj.randint(1, 2)
-        # if bez_rezultatu == 20000:
-        #     print('przelaczam na insert')
-        #     wybor = 1
-        #
+        ## proba modyfikacji nr 5
         # '''if bez_rezultatu == 30000 and ilosc_interacji<80000:
         #     list_zad_los=[]
         #     for x in range(ilosc_zadan):  # tworzenie listy zadan od 0 do tyle ile zadan
@@ -493,6 +455,7 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
         #     print('Losowanie od nowa!')
         #     bez_rezultatu=0'''
         #
+        ## proba modyfikacji nr 6
         # if math.fmod(bez_rezultatu,2500) == 0:
         #     if bez_rezultatu >= 10000:
         #         wyb = 1
@@ -506,8 +469,10 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
         #         print('tryb pdf')
         #         wyb=1
 
+        #po 5k iterecji wyswietlij, ze juz minelo tyle iteracji
         if math.fmod(ilosc_interacji, 5000) == 0:
             print('Iteracja:',ilosc_interacji)
+
         #etap 4 schladzanie
         ilosc_interacji += 1
         if wspol_schladzania == 0: #jesli nie wybrano wspolczynnika schladzania
@@ -516,9 +481,9 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
             schladzanie=wspol_schladzania*temperatura
 
         temperatura = temperatura - schladzanie #obliczenie aktualnej temperatury
-        # print(temperatura)
+        # print(temperatura) #diagnostyka
 
-        #etap 5 ilosc_interacji = 60000 lub break ze wzgledu na niska temp.
+        #etap 5 ilosc_interacji = 130000 lub break ze wzgledu na niska temp.
 
     end=time.time() #koniec wykonywania alg.
     czas=round(end-start,2)  #czas dzialania
@@ -532,18 +497,41 @@ def sym_wyz(ilosc_zadan,temperatura,wspol_schladzania,wybor):
 
     if wspol_schladzania !=0:
         print('Uzyty wspol. schladzania to:',wspol_schladzania,'\n')
-
 #######################################
-
-# sym_wyz(len(kolejka_zadan))
 
 
 sym_wyz(len(kolejka_zadan),3e+400,0,1)
+
+# wybor=1 lub 2 (ost. opcja w fun. powyzej) - wybor dzialania alg: 1 - insert, 2 - swap
+
+###################
+## testowane temp.:
+###################
+# temperatura = 3e+400
+# temperatura = 3e+300
+# temperatura = 3e+1
+# temperatura = 3e-100
+
+############################
+## testowane wspol. chlodz.:
+############################
+
+# wspol_schladzania = 0.99 #wplywa na ilosc iteracji, czym mniejszy tym wiecej iteracji
+# wspol_schladzania = 0.95
+# wspol_schladzania = 0.9
+# wspol_schladzania = 0.8
+# wspol_schladzania = 0.4
+# wspol_schladzania = 0.01
+# wspol_schladzania = 0.01
+
+
+# do testowania:
+# sym_wyz(len(kolejka_zadan),3e+400,0,1)
 # sym_wyz(len(kolejka_zadan),3e+300,0,1)
 # sym_wyz(len(kolejka_zadan),3e+400,0,2)
 # sym_wyz(len(kolejka_zadan),3e+300,0,2)
-
-
+# print('0000000')
+# print('-------')
 # sym_wyz(len(kolejka_zadan),3e+400,0.99,1)
 # sym_wyz(len(kolejka_zadan),3e+300,0.99,1)
 # sym_wyz(len(kolejka_zadan),3e+1,0.99,1)
@@ -590,8 +578,6 @@ sym_wyz(len(kolejka_zadan),3e+400,0,1)
 # print('-------')
 # print('-------\n\n')
 #
-#
-#
 # sym_wyz(len(kolejka_zadan),3e+400,0.99,2)
 # sym_wyz(len(kolejka_zadan),3e+300,0.99,2)
 # sym_wyz(len(kolejka_zadan),3e+1,0.99,2)
@@ -631,25 +617,5 @@ sym_wyz(len(kolejka_zadan),3e+400,0,1)
 # sym_wyz(len(kolejka_zadan),3e+300,0,2)
 # sym_wyz(len(kolejka_zadan),3e+1,0,2)
 # sym_wyz(len(kolejka_zadan),(3e-100),0,2)
-
-
-
-# wybor=1 #wybor dzialania alg: 1 - insert, 2 - swap
-
-# wybor temp wplywa jasno na ilosc ogolnej iteracji
-# temperatura = 3e+400
-# temperatura = 3e+300
-# temperatura = 3e+1
-# temperatura = 3e-100
-
-# wspol_schladzania = 0.99 #wplywa na ilosc iteracji, czym mniejszy tym wiecej iteracji
-# wspol_schladzania = 0.95
-# wspol_schladzania = 0.9
-# wspol_schladzania = 0.8
-# wspol_schladzania = 0.4
-# wspol_schladzania = 0.01
-# wspol_schladzania = 0.01
-
-
 
 print("\nIlosc zadan to",ilosc_zadan,"a ilosc maszyn to",ilosc_maszyn)
