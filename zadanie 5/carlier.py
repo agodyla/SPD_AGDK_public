26c8ddbcdc845d7cc90a8a41abb26b72f142169b
Hirerov <37737923+Hirerov@users.noreply.github.com>
2019-06-01 21:18:49 +0200

    Update carlier.py

--- zadanie 5/carlier.py
+++ zadanie 5/carlier.py
@@ -1,2 +1,3 @@
-#Prace nadal w toku. Przepraszamy za spóżnienie i prosimy o wyrozumiałość. 
+#Prace nadal w toku. Przepraszamy za spóżnienie i prosimy o wyrozumiałość,
+#planuje sie oddanie programu na kolejny poniedziałek - 10.06
 
